ff9b514abad6fa22608f67ecb8cf1b9c1baffed8
elzaw <hoodaelzaw81@gamil.com>
2024-03-24 21:03:45 +0200

    update subcategory

--- controllers/subcategoryController.js
+++ controllers/subcategoryController.js
@@ -4,8 +4,6 @@ const asyncHandler = require("express-async-handler");
 //create
 const createSubCategory = asyncHandler(async (req, res) => {
   const newSubCategory = req.body;
-  const subcategoryCount = await Subcategory.countDocuments();
-  newSubCategory.subcategory_id = subcategoryCount + 1;
 
   const createdSubCategory = await Subcategory.create(newSubCategory);
   res.status(201).json(createdSubCategory);
@@ -22,40 +20,38 @@ const updateCategoryById = asyncHandler(async (req, res) => {
   const id = req.params.id;
   const updates = req.body;
 
-  const subcategory = await Subcategory.findOne({ subcategory_id: id });
+  const subcategory = await Subcategory.findById(id);
 
   if (!subcategory) {
     return res.json({ error: "Subcategory not found" });
   }
 
-  const updatedSubcategory = await Subcategory.findOneAndUpdate(
-    { subcategory_id: id },
-    updates,
-    { new: true }
-  );
+  const updatedSubcategory = await Subcategory.findByIdAndUpdate(id, updates, {
+    new: true,
+  });
 
   res.json(updatedSubcategory);
 });
 
-//get all
+//get by id
 const getSubCategoryById = asyncHandler(async (req, res) => {
   const id = req.params.id;
 
-  const foundSubCategory = await Subcategory.findOne({ subcategory_id: id });
+  const foundSubCategory = await Subcategory.findById(id).populate(
+    "category_id"
+  );
   if (!foundSubCategory) {
     return res.status(400).json("No SubCategory with that ID");
   }
 
-  res.status(200).json(foundSubCategory).populate({ path: "category_id" });
+  res.status(200).json(foundSubCategory);
 });
 
 //del by id
 const deleteSubCategory = asyncHandler(async (req, res) => {
   const id = req.params.id;
 
-  const deletedSubCategory = await Subcategory.findOneAndDelete({
-    subcategory_id: id,
-  });
+  const deletedSubCategory = await Subcategory.findByIdAndDelete(id);
 
   if (!deletedSubCategory) {
     return res.status(404).json({ error: "Subcategory not found" });
